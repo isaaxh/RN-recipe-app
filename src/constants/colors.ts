2bc61e9b9cca6834757dd3c8908e2592d3cfedfc
Isaac Hussain <isaaclearns58@gmail.com>
2024-01-16 10:33:51 +0300

    feat(color): add colors to colors constant file

---
+++ src/constants/colors.ts
@@ -0,0 +1,12 @@
+const COLORS = {
+  primary: '#fefffe',
+  secondary: '#f7f8fe',
+  accent: '#fe6601',
+  white: '#FFFFF0',
+  black: '#333333',
+  placeholder: '#ACACAE',
+  error: '#F44336',
+  success: '#4CAF50',
+};
+
+export default COLORS;
